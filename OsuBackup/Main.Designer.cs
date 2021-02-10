ebe35b5bedc6922263fdfccd6f72ffc48b9a35a8
ImSoSx <32038148+ImSoSx@users.noreply.github.com>
2021-02-10 15:51:49 -0300

    first attempt

---
+++ OsuBackup/Main.Designer.cs
@@ -0,0 +1,222 @@
+﻿
+namespace OsuBackup
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.gameLocation_txbx = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.backupGame_btn = new System.Windows.Forms.Button();
+            this.backupData_btn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataLocation_txbx = new System.Windows.Forms.TextBox();
+            this.gameLocation_btn = new System.Windows.Forms.Button();
+            this.dataLocation_btn = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.autoBackup_btn = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.restoreGame_btn = new System.Windows.Forms.Button();
+            this.restoreData_btn = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // gameLocation_txbx
+            // 
+            this.gameLocation_txbx.Location = new System.Drawing.Point(147, 35);
+            this.gameLocation_txbx.Name = "gameLocation_txbx";
+            this.gameLocation_txbx.Size = new System.Drawing.Size(284, 20);
+            this.gameLocation_txbx.TabIndex = 1;
+            this.gameLocation_txbx.Text = "Write manually or double click to open folder selector";
+            this.gameLocation_txbx.TextChanged += new System.EventHandler(this.gameLocation_txbx_TextChanged);
+            this.gameLocation_txbx.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.gameLocation_txbx_MouseDoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 2;
+            // 
+            // backupGame_btn
+            // 
+            this.backupGame_btn.Location = new System.Drawing.Point(147, 131);
+            this.backupGame_btn.Name = "backupGame_btn";
+            this.backupGame_btn.Size = new System.Drawing.Size(110, 23);
+            this.backupGame_btn.TabIndex = 3;
+            this.backupGame_btn.Text = "Backup Game Only";
+            this.backupGame_btn.UseVisualStyleBackColor = true;
+            this.backupGame_btn.Click += new System.EventHandler(this.backupGame_btn_Click);
+            // 
+            // backupData_btn
+            // 
+            this.backupData_btn.Location = new System.Drawing.Point(398, 131);
+            this.backupData_btn.Name = "backupData_btn";
+            this.backupData_btn.Size = new System.Drawing.Size(114, 23);
+            this.backupData_btn.TabIndex = 4;
+            this.backupData_btn.Text = "Backup Data Only";
+            this.backupData_btn.UseVisualStyleBackColor = true;
+            this.backupData_btn.Click += new System.EventHandler(this.backupData_btn_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            // 
+            // dataLocation_txbx
+            // 
+            this.dataLocation_txbx.Location = new System.Drawing.Point(147, 81);
+            this.dataLocation_txbx.Name = "dataLocation_txbx";
+            this.dataLocation_txbx.Size = new System.Drawing.Size(284, 20);
+            this.dataLocation_txbx.TabIndex = 7;
+            this.dataLocation_txbx.Text = "Write manually or double click to open folder selector";
+            this.dataLocation_txbx.TextChanged += new System.EventHandler(this.dataLocation_txbx_TextChanged);
+            this.dataLocation_txbx.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.dataLocation_txbx_MouseDoubleClick);
+            // 
+            // gameLocation_btn
+            // 
+            this.gameLocation_btn.Location = new System.Drawing.Point(437, 32);
+            this.gameLocation_btn.Name = "gameLocation_btn";
+            this.gameLocation_btn.Size = new System.Drawing.Size(75, 23);
+            this.gameLocation_btn.TabIndex = 8;
+            this.gameLocation_btn.Text = "Save";
+            this.gameLocation_btn.UseVisualStyleBackColor = true;
+            this.gameLocation_btn.Click += new System.EventHandler(this.gameLocation_btn_Click);
+            // 
+            // dataLocation_btn
+            // 
+            this.dataLocation_btn.Location = new System.Drawing.Point(437, 79);
+            this.dataLocation_btn.Name = "dataLocation_btn";
+            this.dataLocation_btn.Size = new System.Drawing.Size(75, 23);
+            this.dataLocation_btn.TabIndex = 9;
+            this.dataLocation_btn.Text = "Save";
+            this.dataLocation_btn.UseVisualStyleBackColor = true;
+            this.dataLocation_btn.Click += new System.EventHandler(this.dataLocation_btn_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(147, 226);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(365, 23);
+            this.progressBar1.TabIndex = 10;
+            // 
+            // autoBackup_btn
+            // 
+            this.autoBackup_btn.Location = new System.Drawing.Point(285, 131);
+            this.autoBackup_btn.Name = "autoBackup_btn";
+            this.autoBackup_btn.Size = new System.Drawing.Size(87, 23);
+            this.autoBackup_btn.TabIndex = 12;
+            this.autoBackup_btn.Text = "Auto Backup";
+            this.autoBackup_btn.UseVisualStyleBackColor = true;
+            this.autoBackup_btn.Click += new System.EventHandler(this.autoBackup_btn_Click);
+            // 
+            // restoreGame_btn
+            // 
+            this.restoreGame_btn.Location = new System.Drawing.Point(147, 181);
+            this.restoreGame_btn.Name = "restoreGame_btn";
+            this.restoreGame_btn.Size = new System.Drawing.Size(110, 23);
+            this.restoreGame_btn.TabIndex = 13;
+            this.restoreGame_btn.Text = "Restore game";
+            this.restoreGame_btn.UseVisualStyleBackColor = true;
+            this.restoreGame_btn.Click += new System.EventHandler(this.restoreGame_btn_Click);
+            // 
+            // restoreData_btn
+            // 
+            this.restoreData_btn.Location = new System.Drawing.Point(398, 181);
+            this.restoreData_btn.Name = "restoreData_btn";
+            this.restoreData_btn.Size = new System.Drawing.Size(114, 23);
+            this.restoreData_btn.TabIndex = 14;
+            this.restoreData_btn.Text = "Restore Data";
+            this.restoreData_btn.UseVisualStyleBackColor = true;
+            this.restoreData_btn.Click += new System.EventHandler(this.restoreData_btn_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(27, 226);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(58, 13);
+            this.linkLabel1.TabIndex = 15;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Tag = "https://github.com/ImSoSx";
+            this.linkLabel1.Text = "By ImSoSx";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 261);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.restoreData_btn);
+            this.Controls.Add(this.restoreGame_btn);
+            this.Controls.Add(this.autoBackup_btn);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.dataLocation_btn);
+            this.Controls.Add(this.gameLocation_btn);
+            this.Controls.Add(this.dataLocation_txbx);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.backupData_btn);
+            this.Controls.Add(this.backupGame_btn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.gameLocation_txbx);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(650, 300);
+            this.MinimumSize = new System.Drawing.Size(650, 300);
+            this.Name = "Main";
+            this.Text = "osu!Lazer backup and restore - by ImSoSx (v.0.1)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox gameLocation_txbx;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button backupGame_btn;
+        private System.Windows.Forms.Button backupData_btn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox dataLocation_txbx;
+        private System.Windows.Forms.Button gameLocation_btn;
+        private System.Windows.Forms.Button dataLocation_btn;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button autoBackup_btn;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Button restoreGame_btn;
+        private System.Windows.Forms.Button restoreData_btn;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
---
+++ OsuBackup/Main.cs
@@ -0,0 +1,408 @@
+﻿using System;
+using System.Windows.Forms;
+using Ionic.Zip;
+using IWshRuntimeLibrary;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+namespace OsuBackup
+{
+    public partial class Main : Form
+    {
+        private string dataLocation = "";
+        private string datalocationLabel = "Select osu! data path:";
+        private int filesExtracted;
+        private string gameLocation = "";
+        private string gamelocationLabel = "Select osu! game path:";
+        private int totalFiles;
+
+        public Main()
+        {
+            InitializeComponent();
+            label1.Text = gamelocationLabel;
+            label2.Text = datalocationLabel;
+            backupGame_btn.Enabled = false;
+            backupData_btn.Enabled = false;
+            toolTip1.SetToolTip(autoBackup_btn, "Try to use the default location from osu");
+            //Validating Restore buttons
+            //Game
+            if (System.IO.File.Exists(Environment.CurrentDirectory + @"\OsuGameBackup.zip"))
+            {
+                restoreGame_btn.Enabled = true;
+            }
+            else
+            {
+                restoreGame_btn.Enabled = false;
+            }
+            //Data
+            if (System.IO.File.Exists(Environment.CurrentDirectory + @"\OsuDataBackup.zip"))
+            {
+                restoreData_btn.Enabled = true;
+            }
+            else
+            {
+                restoreData_btn.Enabled = false;
+            }
+            ///////////////////////////////
+        }
+
+        public void SaveProgress(object sender, SaveProgressEventArgs e)
+        {
+            if (e.EventType == ZipProgressEventType.Saving_Started)
+            {
+            }
+            else if (e.EventType == ZipProgressEventType.Saving_BeforeWriteEntry)
+            {
+                progressBar1.Maximum = e.EntriesTotal;
+                progressBar1.Value = e.EntriesSaved + 1;
+            }
+            else if (e.EventType == ZipProgressEventType.Saving_EntryBytesRead)
+            {
+            }
+            else if (e.EventType == ZipProgressEventType.Saving_Completed)
+            {
+                backupGame_btn.Enabled = true;
+                backupData_btn.Enabled = true;
+            }
+        }
+
+        public void zip_ExtractProgress(object sender, ExtractProgressEventArgs e)
+        {
+            if (e.EventType == ZipProgressEventType.Extracting_BeforeExtractEntry)
+            {
+                filesExtracted++;
+                progressBar1.Value = 100 * filesExtracted / totalFiles;
+            }
+        }
+
+        private void autoBackup_btn_Click(object sender, EventArgs e)
+        {
+            //Use the defaut osu location
+            string osuGame = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\osulazer";
+            string osuData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\osu";
+            autoBackup_btn.Enabled = false;
+            backupData_btn.Enabled = false;
+            backupGame_btn.Enabled = false;
+            gameLocation_txbx.Text = osuGame;
+            dataLocation_txbx.Text = osuData;
+            string osuValG = osuGame + @"\osu!.exe";
+            string osuValD = osuData + @"\client.db";
+            if (System.IO.File.Exists(osuValG))
+            {
+                //Compress osu Game
+                using (var zip = new ZipFile())
+                {
+                    zip.SaveProgress += SaveProgress;
+                    zip.StatusMessageTextWriter = System.Console.Out;
+                    zip.AddDirectory(osuGame, "osulazer");
+                    zip.Save("OsuGameBackup.zip");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Not osu detected in the selected folder");
+                gameLocation = "";
+            }
+            if (System.IO.File.Exists(osuValD))
+            {
+                //Compress osu data
+                using (var zip = new ZipFile())
+                {
+                    zip.SaveProgress += SaveProgress;
+                    zip.StatusMessageTextWriter = System.Console.Out;
+                    zip.AddDirectory(osuData, "osu");
+                    zip.Save("OsuDataBackup.zip");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Not osu detected in the selected folder");
+                gameLocation = "";
+            }
+            //Validating Restore buttons
+            //Game
+            if (System.IO.File.Exists(Environment.CurrentDirectory + @"\OsuGameBackup.zip"))
+            {
+                restoreGame_btn.Enabled = true;
+            }
+            else
+            {
+                restoreGame_btn.Enabled = false;
+            }
+            //Data
+            if (System.IO.File.Exists(Environment.CurrentDirectory + @"\OsuDataBackup.zip"))
+            {
+                restoreData_btn.Enabled = true;
+            }
+            else
+            {
+                restoreData_btn.Enabled = false;
+            }
+            ///////////////////////////////
+        }
+
+        private void backupData_btn_Click(object sender, EventArgs e)
+        {
+            backupData_btn.Enabled = false;
+            backupGame_btn.Enabled = false;
+            string osuVal = dataLocation + @"\client.db";
+            if (System.IO.File.Exists(osuVal))
+            {
+                using (var zip = new ZipFile())
+                {
+                    zip.SaveProgress += SaveProgress;
+                    zip.StatusMessageTextWriter = System.Console.Out;
+                    zip.AddDirectory(dataLocation, "osu");
+                    zip.Save("OsuDataBackup.zip");
+                }
+            }
+            else
+            {
+                Console.WriteLine(osuVal);
+                MessageBox.Show("Not osu detected in the selected folder");
+                gameLocation = "";
+            }
+
+            //Validating Restore buttons
+            //Game
+            if (System.IO.File.Exists(Environment.CurrentDirectory + @"\OsuGameBackup.zip"))
+            {
+                restoreGame_btn.Enabled = true;
+            }
+            else
+            {
+                restoreGame_btn.Enabled = false;
+            }
+            //Data
+            if (System.IO.File.Exists(Environment.CurrentDirectory + @"\OsuDataBackup.zip"))
+            {
+                restoreData_btn.Enabled = true;
+            }
+            else
+            {
+                restoreData_btn.Enabled = false;
+            }
+            ///////////////////////////////
+        }
+
+        private void backupGame_btn_Click(object sender, EventArgs e)
+        {
+            //Disable buttons until finish the zip process
+            backupGame_btn.Enabled = false;
+            backupData_btn.Enabled = false;
+            string osuVal = gameLocation + @"\osu!.exe";
+            if (System.IO.File.Exists(osuVal))
+            {
+                using (var zip = new ZipFile())
+                {
+                    zip.SaveProgress += SaveProgress;
+                    zip.StatusMessageTextWriter = System.Console.Out;
+                    zip.AddDirectory(gameLocation, "osulazer");
+                    zip.Save("OsuGameBackup.zip");
+                }
+            }
+            else
+            {
+                Console.WriteLine(osuVal);
+                MessageBox.Show("Not osu detected in the selected folder");
+                gameLocation = "";
+            }
+
+            //Validating Restore buttons
+            //Game
+            if (System.IO.File.Exists(Environment.CurrentDirectory + @"\OsuGameBackup.zip"))
+            {
+                restoreGame_btn.Enabled = true;
+            }
+            else
+            {
+                restoreGame_btn.Enabled = false;
+            }
+            //Data
+            if (System.IO.File.Exists(Environment.CurrentDirectory + @"\OsuDataBackup.zip"))
+            {
+                restoreData_btn.Enabled = true;
+            }
+            else
+            {
+                restoreData_btn.Enabled = false;
+            }
+            ///////////////////////////////
+        }
+
+        private void dataLocation_btn_Click(object sender, EventArgs e)
+        {
+            if (dataLocation_txbx.Text.Length <= 3)
+            {
+                backupData_btn.Enabled = false;
+            }
+            else
+            {
+                backupData_btn.Enabled = true;
+            }
+            dataLocation = dataLocation_txbx.Text;
+        }
+
+        private void dataLocation_txbx_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            dataLocation_txbx.Text = "";
+            //Disable button if textbox for game path is empty
+            if (dataLocation_txbx.Text.Length <= 0)
+            {
+                backupData_btn.Enabled = false;
+            }
+            else
+            {
+                backupData_btn.Enabled = true;
+            }
+
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.InitialDirectory = "C:\\Users";
+            dialog.IsFolderPicker = true;
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                dataLocation = dialog.FileName;
+                string osuVal = dataLocation + @"\client.db";
+                if (System.IO.File.Exists(osuVal))
+                {
+                    dataLocation = dialog.FileName;
+                    dataLocation_txbx.Text = dataLocation;
+                }
+                else
+                {
+                    Console.WriteLine(osuVal);
+                    MessageBox.Show("Not osu data detected in the selected folder");
+                    dataLocation = "";
+                }
+            }
+        }
+
+        private void dataLocation_txbx_TextChanged(object sender, EventArgs e)
+        {
+            if (dataLocation_txbx.Text.Length <= 3)
+            {
+                backupData_btn.Enabled = false;
+            }
+            else
+            {
+                backupData_btn.Enabled = true;
+            }
+        }
+
+        private void gameLocation_btn_Click(object sender, EventArgs e)
+        {
+            if (gameLocation_txbx.Text.Length <= 3)
+            {
+                backupGame_btn.Enabled = false;
+            }
+            else
+            {
+                backupGame_btn.Enabled = true;
+            }
+            gameLocation = gameLocation_txbx.Text;
+        }
+
+        private void gameLocation_txbx_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            gameLocation_txbx.Text = "";
+            //Disable button if textbox for game path is empty
+            if (gameLocation_txbx.Text.Length <= 0)
+            {
+                backupGame_btn.Enabled = false;
+            }
+            else
+            {
+                backupGame_btn.Enabled = true;
+            }
+
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.InitialDirectory = "C:\\Users";
+            dialog.IsFolderPicker = true;
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                gameLocation = dialog.FileName;
+                string osuVal = gameLocation + @"\osu!.exe";
+                if (System.IO.File.Exists(osuVal))
+                {
+                    gameLocation = dialog.FileName;
+                    gameLocation_txbx.Text = gameLocation;
+                }
+                else
+                {
+                    Console.WriteLine(osuVal);
+                    MessageBox.Show("Not osu detected in the selected folder");
+                    gameLocation = "";
+                }
+            }
+        }
+
+        private void gameLocation_txbx_TextChanged(object sender, EventArgs e)
+        {
+            if (gameLocation_txbx.Text.Length <= 3)
+            {
+                backupGame_btn.Enabled = false;
+            }
+            else
+            {
+                backupGame_btn.Enabled = true;
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/ImSoSx");
+        }
+
+        private void restoreData_btn_Click(object sender, EventArgs e)
+        {
+            autoBackup_btn.Enabled = false;
+            restoreGame_btn.Enabled = false;
+            restoreData_btn.Enabled = false;
+            backupGame_btn.Enabled = false;
+            backupData_btn.Enabled = false;
+
+            string osuData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            using (ZipFile zip = ZipFile.Read(Environment.CurrentDirectory + @"\OsuDataBackup.zip"))
+            {
+                totalFiles = zip.Count;
+                filesExtracted = 0;
+                zip.ExtractProgress += zip_ExtractProgress;
+                zip.ExtractAll(osuData, ExtractExistingFileAction.OverwriteSilently);
+            }
+            autoBackup_btn.Enabled = true;
+            restoreGame_btn.Enabled = true;
+            restoreData_btn.Enabled = true;
+            backupGame_btn.Enabled = true;
+            backupData_btn.Enabled = true;
+        }
+
+        private void restoreGame_btn_Click(object sender, EventArgs e)
+        {
+            autoBackup_btn.Enabled = false;
+            restoreGame_btn.Enabled = false;
+            restoreData_btn.Enabled = false;
+            backupGame_btn.Enabled = false;
+            backupData_btn.Enabled = false;
+            string osuGame = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            using (ZipFile zip = ZipFile.Read(Environment.CurrentDirectory + @"\OsuGameBackup.zip"))
+            {
+                totalFiles = zip.Count;
+                filesExtracted = 0;
+                zip.ExtractProgress += zip_ExtractProgress;
+                zip.ExtractAll(osuGame, ExtractExistingFileAction.OverwriteSilently);
+            }
+
+            object shDesktop = (object)"Desktop";
+            WshShell shell = new WshShell();
+            string shortcutAddress = (string)shell.SpecialFolders.Item(ref shDesktop) + @"\osu!Lazer.lnk";
+            IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(shortcutAddress);
+            shortcut.TargetPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\osulazer\osu!.exe";
+            shortcut.Save();
+
+            autoBackup_btn.Enabled = true;
+            restoreGame_btn.Enabled = true;
+            restoreData_btn.Enabled = true;
+            backupGame_btn.Enabled = true;
+            backupData_btn.Enabled = true;
+        }
+    }
+}
